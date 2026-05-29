bb19b9f480b0ffa82cbcd9b41eb319cce1c94564
Hyrcanius <vicent.perezv@duocuc.cl>
2026-05-29 16:45:43 -0400

    Creación del menú y primera función

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,51 @@
-print("hola hola")
+productos = {}
+op = 0
+
+def agregar_producto(productos):
+    nombre = input("Nombre del producto: ").strip()
+
+    if nombre == "":
+        print("El nombre no puede ser vacío")
+        return
+    
+    if "" in nombre:
+        print("No pueden haber espacios en el nombre")
+        return
+    
+    if nombre in productos:
+        print("El producto ya existe.")
+        return
+
+    stock = int(input("Ingrese stock: "))
+    precio = int(input("Ingrese precio: "))
+
+    productos[nombre] = [stock, precio]
+
+while True:
+    try:
+        print("----MENU----")
+        print("1. Agregar producto")
+        print("2. Mostrar productos")
+        print("3. Buscar productos")
+        print("4. Producto más caro")
+        print("5. Salir")
+        op = int(input("Ingrese una opción: "))
+
+        if op == 1:
+            agregar_producto(productos)
+        elif op == 2:
+            #mostrar_productos(productos)#
+            print(productos)
+        elif op == 3:
+            #buscar_producto(productos)#
+            print("buscando")
+        elif op == 4:
+            #producto_mas_caro(productos)#
+            print("carozzi")
+        elif op == 5:
+            print("Gracias por utilizar nuestro software.")
+            break
+        else:
+            print("Ingrese una opción válida.")
+    except:
+        print("Opción inválida.")
