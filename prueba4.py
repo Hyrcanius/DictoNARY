0f7baac109afd8e7c3b5f73ba5c7841ebb3a207d
Hyrcanius <vicent.perezv@duocuc.cl>
2026-06-10 14:57:51 -0400

    implementación de diccionario para ingreso de usuario

--- prueba4.py
+++ prueba4.py
@@ -10,6 +10,20 @@ usuarios = {
     "Marge": ["F", "mmhomero69"]
 }
 
+usuarios2 = {
+    "Moe": {
+        "sexo": "M",
+        "pass": "barneymylove"
+    },
+    "Barney": {
+        "sexo": "M",
+        "pass": "moemylove666"
+    },
+    "Edna": {
+        "sexo": "F",
+        "pass": "crabaple23"
+    } 
+}
 def ingresar_usuario(usuarios):
     nombre_usr = input("Ingrese el nombre de usuario: ").capitalize().strip()
     if nombre_usr in usuarios:
@@ -23,19 +37,56 @@ def ingresar_usuario(usuarios):
         else:
             print("Ingreso inválido, use 'F' o 'M'")
 
-    contra = input("Ingrese la contraseña del usuario: ").strip()
-    cont = 0
-    if len(contra) > 8:
-        for i in range(len(contra)):
-            pos = contra[i]
-            if (pos == "1") or (pos == "2") or (pos == "3") or (pos == "4") or (pos == "5") or (pos == "6") or (pos == "7") or (pos == "8") or (pos == "9") or (pos == "0"):
-                cont = cont + 1
-        if cont > 0:
-            if " " in contra:
-                print("Ingreso inválido")
-                return
+    while True:
+        contra = input("Ingrese la contraseña del usuario: ").strip()
+        cont = 0
+        if len(contra) > 8:
+            for i in range(len(contra)):
+                pos = contra[i]
+                if pos.isdigit() == True:
+                    cont = cont + 1
+            if cont > 0:
+                if " " in contra:
+                    print("Ingreso inválido")
+                else:
+                    usuarios[nombre_usr] = [sexo, contra]
+                    break
             else:
-                usuarios[nombre_usr] = [sexo, contra]
+                print("Contraseña debe tener al menos un número.")
+
+
+def ingresar_usuario2(usuarios2):
+    nombre_usr = input("Ingrese el nombre de usuario: ").strip().capitalize()
+    if nombre_usr in usuarios2:
+        print("Nombre en uso.")
+        return
+    
+    while True:
+        sexo = input("Ingrese el sexo (F o M): ")
+        if (sexo == "F") or (sexo == "M"):
+            break
+        else:
+            print("Ingrese una opción válida.")
+    
+    while True:
+        contra = input("Ingrese la contraseña: ").strip()
+        cont = 0
+        if len(contra) > 8:
+            for i in range(len(contra)):
+                pos = contra[i]
+                if pos.isdigit() == True:
+                    cont = cont + 1
+            if cont > 0:
+                if " " in contra:
+                    print("Contraseña inválida.")
+                else:
+                    usuarios[nombre_usr] = {
+                        "sexo": sexo,
+                        "contraseña": contra 
+                    }
+                    break
+            else:
+                print("Contraseña debe tener al menos un número.")
 
 def buscar_usuario(usuarios):
     nombre = input("Ingrese el nombre de usuario: ").capitalize().strip()
