8bb0feb5242d5acd6524a5be94fc766bb77d5654
Hyrcanius <vicent.perezv@duocuc.cl>
2026-06-12 16:00:59 -0400

    Cambios en ayudantia

--- ejercicio2ayudantia.py
+++ ejercicio2ayudantia.py
@@ -3,28 +3,28 @@ nombre_buscado = ""
 
 def pedir_opcion():
     print("[A] Agregar contacto | [B] Buscar | [S] Salir")
-    op = input("Ingrese una opción: ")
-    op.strip().capitalize()
+    op = input("Ingrese una opción: ").strip().capitalize()
     return op
 
 contactos = {}
 
 def buscar_numero(contactos, nombre_buscado):
-    nombre_buscado = input("Ingrese el nombre del usuario: ")
+    nombre_buscado = input("Ingrese el nombre del usuario: ").strip().capitalize()
     if nombre_buscado in contactos:
-        numero = contactos.items(nombre_buscado)
+        numero = contactos[nombre_buscado]
         return numero
     else:
         print("El contacto no existe.")
 
 while True:
-    pedir_opcion()
+    op = pedir_opcion()
     if op == "A":
         nombre = input("Ingrese el nombre: ").strip().capitalize()
         telefono = input("Ingrese el número de teléfono: ").strip()
         contactos[nombre] = telefono
     elif op == "B":
-        buscar_numero(contactos, nombre_buscado)
+        resultado = buscar_numero(contactos, nombre_buscado)
+        print(resultado)
     elif op == "S":
         print("Saliendo....")
         break
